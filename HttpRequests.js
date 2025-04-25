5818911174b018d99864b7c580c17c3880241c82
Daniel Shumbusho <126154957+DanielShumbusho@users.noreply.github.com>
2025-04-25 16:16:15 +0200

    wrapped inside a funcion

--- HttpRequests.js
+++ HttpRequests.js
@@ -1,18 +1,21 @@
-// //we will be doing api requests 
-// // and GET back some json data
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-//   .then(response => response.json()) // convert the response to json
-//   .then(json => console.log(json)) // log the json data to the console
-const request = new XMLHttpRequest(); // create a new XMLHttpRequest object
+const getTodos = () => {
+    //we will be doing api requests 
+    //and GET back some json data
+    // fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //.then(response => response.json()) // convert the response to json
+    //.then(json => console.log(json)) // log the json data to the console
+    const request = new XMLHttpRequest(); // create a new XMLHttpRequest object
 
-request.addEventListener('readystatechange', () =>{
-    //console.log(request, request.readyState); // log the current state of the request
-    if(request.readyState === 4 && request.status === 200) {
-        console.log(request.responseText); // log the response text when the request is complete
-    }else if (request.readyState === 4){
-        console.log('could not fetch data'); // log an error message if the request fails
-    }
-});
+    request.addEventListener('readystatechange', () =>{
+        //console.log(request, request.readyState); // log the current state of the request
+        if(request.readyState === 4 && request.status === 200) {
+            console.log(request.responseText); // log the response text when the request is complete
+        }else if (request.readyState === 4){
+            console.log('could not fetch data'); // log an error message if the request fails
+        }
+    });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/'); // open a GET request to the specified URL
-request.send(); // send the request
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/'); // open a GET request to the specified URL
+    request.send(); // send the request
+}
+
