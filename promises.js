99d84de9ac11894f3475e7cc5713e1250a5fa112
Daniel Shumbusho <126154957+DanielShumbusho@users.noreply.github.com>
2025-04-25 18:40:41 +0200

    basic promise implementation

--- promises.js
+++ promises.js
@@ -1,28 +1,47 @@
-const getTodos = (resource, callback) => {//'callback' can be called anything
-    const request = new XMLHttpRequest(); // create a new XMLHttpRequest object
+const getTodos = (resource) => {//'callback' can be called anything
 
-    request.addEventListener('readystatechange', () =>{
-        if(request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText)
-            callback(undefined, data); 
-        }else if (request.readyState === 4){
-            callback('could not fetch data', undefined);//calls the callbak function with two parameters(err, data as undefined)
-        }
-    });
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest(); // create a new XMLHttpRequest object
 
+        request.addEventListener('readystatechange', () =>{
+            if(request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText)
+                resolve(data) 
+            }else if (request.readyState === 4){
+                reject('error getting resources');//calls the callbak function with two parameters(err, data as undefined)
+            }
+        });
     
-    request.open('GET', resource);
-    request.send(); 
-}
-//the long way of doing it
-//callback hell
-//the rest is in promises.js
-getTodos('todos/luigi.json',(err, data) =>{
-    console.log(data);
-    getTodos('todos/mario.json',(err, data) =>{
-        console.log(data);
-        getTodos('todos/shawn.json',(err, data) =>{
-            console.log(data);
-        })
-    })
-}); 
+        
+        request.open('GET', resource);
+        request.send();  
+    });
+    
+};
+
+getTodos('todos/luigi.json').then(data => {
+    console.log('promise resolved', data)
+}).catch(err => {
+    console.log('promise rejected', err);
+})
+
+//promise example
+// const getSomething = () => {
+//     return new Promise((resolve, reject) => {
+//         //fetch something
+//         // resolve ('some data');
+//         reject('some error');
+//     });
+// }
+// getSomething().then((data) => {
+//     console.log(data);
+// }, (err) => {
+//     console.log(err)
+// });
+
+//a different way
+// getSomething().then(data => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// });
