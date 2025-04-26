e45b10e348397a54781f753b92f0324e2d48faa5
Daniel Shumbusho <126154957+DanielShumbusho@users.noreply.github.com>
2025-04-26 09:46:23 +0200

    error handling in fetch

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,11 +1,18 @@
 
 const getTodos = async () => {
-    const response = await fetch('todos/luigi.json');
+    const response = await fetch('todos/luigi.json');//but in case of typo the fetch doesn't reject
+    //instead will give a status of 404 so we will check the status
+    //and explicitely throw an error using the throw method
+    if (response.status !== 200){//if the response's status is not OK then
+        throw new Error('Cannot fetch data')
+    }
     const data = await response.json();
     return data
 }
 
-const test = getTodos()
+getTodos()
+    .then(data => console.log('resolved: ', data))
+    .catch(err => console.log('rejected: ', err))
 
 // const test = getTodos();//this will be a new promise
 // console.log(test)
